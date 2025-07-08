2f4f55f0a7b5739a41711f4a9d1fe86644d004b2
Elsie Oduor <eoatieno@gmail.com>
2025-07-08 15:04:37 +0300

    added the cors

--- src/config/cron.js
+++ src/config/cron.js
@@ -1,14 +1,41 @@
-import cron from 'cron'
-import https from 'https'
+// import cron from 'cron'
+// import https from 'https'
 
-const job = new cron.CronJob("*/14 * * * *", function (){
-    https.get(process.env.API_URL, (res)=>{
-        if (res.statusCode ===200) console.log('GET request sent successfully')
-            else console.log('Error sending GET request', res.statusCode)
-    }).on("error", (e)=> console.error("Error while sending request", e))
-})
+// const job = new cron.CronJob("*/14 * * * *", function (){
+//     https.get(process.env.API_URL, (res)=>{
+//         if (res.statusCode ===200) console.log('GET request sent successfully')
+//             else console.log('Error sending GET request', res.statusCode)
+//     }).on("error", (e)=> console.error("Error while sending request", e))
+// })
 
-export default job
+// export default job
+import cron from 'cron';
+import https from 'https';
+
+const job = new cron.CronJob('*/14 * * * *', function () {
+  const options = {
+    hostname: 'recipe-app-w5x7.onrender.com',
+    path: '/',
+    method: 'GET'
+  };
+
+  const req = https.request(options, (res) => {
+    if (res.statusCode === 200) {
+      console.log('GET request sent successfully');
+    } else {
+      console.log('Error sending GET request', res.statusCode);
+    }
+  });
+
+  req.on('error', (e) => {
+    console.error('Error while sending request', e);
+  });
+
+  req.end();
+});
+
+export default job;
+
 
 // CRON JOB EXPLANATION:
 // Cron jobs are scheduled tasks that run periodically at fixed intervals
